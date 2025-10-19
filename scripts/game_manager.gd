012c4ecf6431d94905621c64d7362a8ef5548603
Gustavo Larsen <guslarsen@gmail.com>
2025-10-18 22:31:09 -0300

    feat: adiciona musica e efeito sonoro

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,9 @@ extends Node
 var _score = 0
 
 @onready var score_label: Label = $"../UI/ScoreLabel"
+@onready var pickup_sound: AudioStreamPlayer2D = $PickupSound
 
 func add_point() -> void:
 	_score += 1
+	pickup_sound.play()
 	score_label.text = "Score: %d" % _score
